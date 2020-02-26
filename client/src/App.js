f6ee172a04d5aa2ee7ac28aa385251ca362184ff
psych0sykes <blakesykes13@gmail.com>
2020-02-26 12:12:35 -0700

    Added react-router-dom and a couple compoents and a home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Home from './pages/Home';
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-      </div>
-    );
-  }
+function App() {
+
+  return(
+    <Router>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+        </Switch>
+    </Router>
+  )
 }
 
 export default App;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Home(props) {
+
+    return(
+        <div>home</div>
+    )
+};
+
+export default Home;
